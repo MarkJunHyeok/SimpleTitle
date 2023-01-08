1e43367caaf1c60e21fcd1d0100fcb834bbbbf84
Jun, Ahah <96584832+peubel@users.noreply.github.com>
2023-01-08 20:29:23 +0900

    타이틀 모델 테스트 오류 해결

--- src/test/java/simple/title/model/TitleTest.java
+++ src/test/java/simple/title/model/TitleTest.java
@@ -13,96 +13,60 @@ class TitleTest {
 
     @Test
     void 칭호_생성() {
-        Title title = Title.create("name", "displayName", "description");
+        Title title = Title.create("name");
 
         assertThat(title.getName()).isEqualTo("name");
-        assertThat(title.getDescription()).isEqualTo("description");
     }
 
     @ParameterizedTest
     @NullAndEmptySource
     void 칭호_생성_실패__이름이_없음(String name) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            Title.create(name, "displayName", "description");
-        });
-    }
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    void 칭호_생성_실패__표시될_이름이_없음(String displayName) {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            Title.create("name", displayName, "description");
-        });
-    }
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    void 칭호_생성_실패__설명이_없음(String description) {
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            Title.create("name", "displayName", description);
+            Title.create(name);
         });
     }
 
     @Test
-    void 칭호_이름_변경() {
-        Title title = Title.create("name", "displayName", "description");
+    void 칭호_표시될_이름_적용() {
+        Title title = Title.create("name");
 
-        title.changeName("changeName");
-
-        assertThat(title.getName()).isEqualTo("changeName");
-    }
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    void 칭호_이름_변경_실패__이름이_공백(String changeName) {
-        Title title = Title.create("name", "displayName", "description");
-
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            title.changeName(changeName);
-        });
-    }
-
-    @Test
-    void 칭호_표시될_이름_변경() {
-        Title title = Title.create("name", "displayName", "description");
-
-        title.changeDisplayName("changeDisplayName");
+        title.setDisplayName("changeDisplayName");
 
         assertThat(title.getDisplayName()).isEqualTo("changeDisplayName");
     }
 
     @ParameterizedTest
     @NullAndEmptySource
-    void 칭호_표시될_이름_변경_실패__표시될_이름이_공백(String changeDisplayName) {
-        Title title = Title.create("name", "displayName", "description");
+    void 칭호_표시될_이름_적용_실패__표시될_이름이_공백(String changeDisplayName) {
+        Title title = Title.create("name");
 
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            title.changeDisplayName(changeDisplayName);
+            title.setDisplayName(changeDisplayName);
         });
     }
 
     @Test
-    void 칭호_설명_변경() {
-        Title title = Title.create("name", "displayName", "description");
+    void 칭호_설명_적용() {
+        Title title = Title.create("name");
 
-        title.changeDescription("changeDescription");
+        title.setDescription("changeDescription");
 
         assertThat(title.getDescription()).isEqualTo("changeDescription");
     }
 
     @ParameterizedTest
     @NullAndEmptySource
-    void 칭호_설명_변경_실패__설명이_공백(String changeDescription) {
-        Title title = Title.create("name", "displayName", "description");
+    void 칭호_설명_적용_실패__설명이_공백(String changeDescription) {
+        Title title = Title.create("name");
 
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            title.changeDescription(changeDescription);
+            title.setDescription(changeDescription);
         });
     }
 
     @Test
     void 칭호_전용_권한_추가() {
-        Title title = Title.create("name", "displayName", "description");
+        Title title = Title.create("name");
 
         Permission permission = new Permission("test.permission");
 
@@ -113,7 +77,7 @@ class TitleTest {
 
     @Test
     void 칭호_전용_권한_추가_실패__이미_있는_권한임() {
-        Title title = Title.create("name", "displayName", "description");
+        Title title = Title.create("name");
 
         Permission permission = new Permission("test.permission");
 
